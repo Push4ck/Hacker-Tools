6b19678b242224b134763ab8d527c91b4a1c204a
Pushkar Sharma <96145925+pushkarsharma23@users.noreply.github.com>
2023-02-05 16:54:39 +0530

    Update and rename arp-scan.py to Snooper.py
    This code uses the subprocess and re libraries in Python to retrieve a list of nearby devices on a network using the arp-scan command. The function get_nearby_devices() captures the output of the arp-scan command, splits it into separate lines, and loops through each line to extract the IP address, MAC address, and hostname of each device. The IP address and MAC address are then validated using regular expressions before being added to a list of devices. The list of devices is returned by the function and printed out at the end of the code.

--- Snooper.py
+++ Snooper.py
@@ -1,21 +1,36 @@
 import subprocess
 import re
 
+# Function to get nearby devices using arp-scan
 def get_nearby_devices():
+    # Run the arp-scan command with --localnet option and capture the output
     output = subprocess.run(["arp-scan", "--localnet"], capture_output=True).stdout.decode()
+
+    # If the output is empty, return an empty list
     if not output:
         return []
+
+    # Split the output into separate lines
     lines = output.split("\n")
     devices = []
+
+    # Loop through each line
     for line in lines:
         parts = line.split()
+
+        # Check if the line has 3 parts (IP address, MAC address, and hostname)
         if len(parts) == 3:
             ip_address = parts[0]
             mac_address = parts[1]
             hostname = parts[2]
+
+            # Check if the IP address and MAC address match the expected format
             if re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip_address) and re.match(r"^[a-fA-F0-9:]{17}$", mac_address):
                 devices.append({"ip_address": ip_address, "mac_address": mac_address, "hostname": hostname})
     return devices
 
+# Get the nearby devices
 devices = get_nearby_devices()
+
+# Print the list of devices
 print(devices)
