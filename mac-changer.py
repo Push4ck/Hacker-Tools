7bc1182d702102861cf60e42bf8d9b7acdb5c771
Pushkar Sharma <96145925+pushkarsharma23@users.noreply.github.com>
2023-02-05 16:05:35 +0530

    Add files via upload

---
+++ mac-changer.py
@@ -0,0 +1,27 @@
+import subprocess
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more information")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new MAC, use --help for more information")
+    return options
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] MAC Address did not get changed")
