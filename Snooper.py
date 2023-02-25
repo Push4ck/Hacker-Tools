9190374a7344b0f33e28621ef08525bb8c8b30fd
Pushkar Sharma <96145925+pushkarsharma23@users.noreply.github.com>
2023-02-26 02:53:11 +0530

    Update Snooper.py

--- Snooper.py
+++ Snooper.py
@@ -1,9 +1,45 @@
-# Import required libraries
 import subprocess
 import re
+import platform
+import os
+import socket
 
-# Function to get nearby devices using arp-scan
+
+def get_os():
+    return platform.system()
+
+
+def is_root():
+    return os.geteuid() == 0
+
+
+def is_connected():
+    try:
+        # Try to connect to Google's DNS server
+        socket.create_connection(("8.8.8.8", 53))
+        return True
+    except OSError:
+        return False
+
+
 def get_nearby_devices():
+    # Check if the user has root privileges
+    if not is_root():
+        print("You need to have root privileges to run this script.")
+        return []
+
+    # Check if the operating system is Linux
+    if get_os() != "Linux":
+        print("This script can only be run on Linux.")
+        return []
+
+    # Check if arp-scan is installed
+    try:
+        subprocess.check_output(["which", "arp-scan"])
+    except subprocess.CalledProcessError:
+        print("arp-scan is not installed. Please install it and try again.")
+        return []
+
     # Run the arp-scan command with --localnet option and capture the output
     output = subprocess.check_output(["arp-scan", "--localnet"]).decode()
 
@@ -30,8 +66,18 @@ def get_nearby_devices():
                 devices.append({"ip_address": ip_address, "mac_address": mac_address, "hostname": hostname})
     return devices
 
-# Get the nearby devices
-devices = get_nearby_devices()
 
-# Print the list of devices
-print(devices)
+# Check if the device is connected to a network
+if not is_connected():
+    print("You are not connected to a network. Please connect to a network and try again.")
+else:
+    # Get the nearby devices
+    devices = get_nearby_devices()
+
+    # Print the list of devices
+    if devices:
+        print("Nearby devices:")
+        for device in devices:
+            print(f"IP address: {device['ip_address']}, MAC address: {device['mac_address']}, Hostname: {device['hostname']}")
+    else:
+        print("No devices found.")
