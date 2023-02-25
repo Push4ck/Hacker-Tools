6ac6445559c98b0ad72984a301376f4302f4fec2
Pushkar Sharma <96145925+pushkarsharma23@users.noreply.github.com>
2023-02-26 01:43:56 +0530

    Update MACSpoofer.py

--- MACSpoofer.py
+++ MACSpoofer.py
@@ -2,26 +2,29 @@ import subprocess
 import optparse
 import re
 
+
 # Define a function to parse command line arguments
 def get_arguments():
     # Create an instance of OptionParser class
     parser = optparse.OptionParser()
 
     # Add options for the interface and new MAC address
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
-    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
+    parser.add_option("-i", "--interface", dest="interface",
+                      help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 
     # Parse the options and arguments
     (options, arguments) = parser.parse_args()
 
     # Check if the interface and new MAC address are specified
     if not options.interface:
-        parser.error("[-] Please specify an interface, use --help for more information")
+        parser.error("[-] Please specify an interface")
     elif not options.new_mac:
-        parser.error("[-] Please specify a new MAC, use --help for more information")
+        parser.error("[-] Please specify a new MAC")
 
     # Return the options
     return options
+
 
 # Define a function to change the MAC address
 def change_mac(interface, new_mac):
@@ -37,13 +40,15 @@ def change_mac(interface, new_mac):
     # Enable the specified interface
     subprocess.call(["ifconfig", interface, "up"])
 
+
 # Define a function to get the current MAC address of an interface
 def get_current_mac(interface):
     # Get the result of running the ifconfig command on the specified interface
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
 
     # Search for the MAC address in the result
-    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",
+                                          ifconfig_result.decode('utf-8'))
 
     # Return the MAC address if found
     if mac_address_search_result:
@@ -51,3 +56,23 @@ def get_current_mac(interface):
     else:
         # Print an error message if the MAC address was not changed
         print("[-] MAC Address did not get changed")
+
+
+# Get the command line arguments
+options = get_arguments()
+
+# Get the current MAC address of the specified interface
+current_mac = get_current_mac(options.interface)
+print("[+] Current MAC Address: " + str(current_mac))
+
+# Change the MAC address of the specified interface
+change_mac(options.interface, options.new_mac)
+
+# Get the current MAC address of the specified interface
+current_mac = get_current_mac(options.interface)
+
+# Check if the MAC address was successfully changed
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[-] MAC address did not get changed")
