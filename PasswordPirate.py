8e58a4c7fa46f3f305f7075490a7253514bad44c
Pushkar Sharma <96145925+pushkarsharma23@users.noreply.github.com>
2023-02-26 00:32:50 +0530

    Update PasswordPirate.py

--- PasswordPirate.py
+++ PasswordPirate.py
@@ -1,57 +1,74 @@
-def password_generator():
-    # Ask the user for words to use in password generation
-    words_input = input("Enter a list of words separated by comma: ")
-    if words_input.strip() == "":
-        raise Exception("Input cannot be empty")
-    words = [word.strip() for word in words_input.split(",")]
+import argparse
+import itertools
+import random
+import string
+import sys
 
-    # List of special characters
-    special_chars = ["!", "@", "#", "$", "%", "^", "&", "*"]
 
-    # Maximum length of the password
-    max_length = input("Enter the maximum length of the password: ")
-    try:
-        max_length = int(max_length)
-    except ValueError:
-        raise Exception("Max length should be a positive integer")
-    if max_length <= 0:
-        raise Exception("Max length should be a positive integer")
+def generate_passwords(keywords, min_length, max_length,
+                       use_special_chars=False, use_numbers=False):
+    """Generate unique passwords from the given keywords and options."""
+    for n in range(min_length, max_length + 1):
+        for keyword in keywords:
+            if len(keyword) >= n:
+                yield keyword[:n]
+            else:
+                for combination in itertools.product(keyword, repeat=n):
+                    yield ''.join(combination)
+    if use_special_chars or use_numbers:
+        for password in generate_passwords(keywords, min_length, max_length):
+            for n in range(1, len(password)):
+                if use_special_chars:
+                    for special_char in string.punctuation:
+                        yield f"{password[:n]}{special_char}{password[n:]}"
+                if use_numbers:
+                    for number in string.digits:
+                        yield password[:n] + number + password[n:]
 
-    # List to store the generated passwords
-    passwords = []
 
-    # Check if the input words contain only alphanumeric characters
-    for word in words:
-        if not word.isalnum():
-            raise Exception("The word should contain only alphanumeric characters")
+def generate_password(length, use_special_chars=False, use_numbers=False):
+    """Generate a random password of the given length."""
+    chars = string.ascii_letters
+    if use_special_chars:
+        chars += string.punctuation
+    if use_numbers:
+        chars += string.digits
+    return ''.join(random.choice(chars) for _ in range(length))
 
-    # Loop through the words
-    for word in words:
-        # Append the word as is to the list
-        passwords.append(word)
 
-        # Loop through the special characters
-        for char in special_chars:
-            # Add the special character to the beginning and end of the word
-            passwords.append(char + word)
-            passwords.append(word + char)
+def parse_args(args):
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser('Generate passwords from keywords.')
+    parser.add_argument('--keywords', metavar='KEYWORD', nargs='+',
+                        help='keywords to use in password generation')
+    parser.add_argument('--min-length', metavar='MIN LENGTH', type=int,
+                        default=1,
+                        help='minimum length of the password (default: 1)')
+    parser.add_argument('--max-length', metavar='MAX LENGTH', type=int,
+                        default=10,
+                        help='maximum length of the password (default: 10)')
+    parser.add_argument('--special-chars', action='store_true', default=False,
+                        help='use special characters in the password')
+    parser.add_argument('--numbers', action='store_true', default=False,
+                        help='use numbers in the password')
+    return parser.parse_args(args)
 
-            # Add the special character to the middle of the word
-            mid = int(len(word) / 2)
-            if len(word) % 2 == 0:
-                mid -= 1
-            passwords.append(word[:mid] + char + word[mid:])
 
-        # Loop through the length of the password
-        for length in range(2, max_length + 1):
-            # Repeat the word to create a new password with the given length
-            new_password = word * length
-            passwords.append(new_password)
+def main(args):
+    """Generate passwords from command line arguments and print them to stdout."""
+    opts = parse_args(args)
 
-    # Remove duplicates from the list of passwords
-    passwords = list(set(passwords))
+    # Ask user for the file path to write the passwords to
+    output_file = input("Enter the file path to write the passwords to: ")
 
-    # Print the generated passwords
-    print(passwords)
+    # Write passwords to the output file
+    with open(output_file, 'w') as f:
+        for password in generate_passwords(opts.keywords, opts.min_length,
+                                           opts.max_length,
+                                           use_special_chars=opts.special_chars,
+                                           use_numbers=opts.numbers):
+            f.write(password + '\n')
 
-password_generator()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
