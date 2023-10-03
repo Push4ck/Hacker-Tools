e08d845601c0ede73152af0b1f33137066f97421
Pushkar Sharma <96145925+Push4ck@users.noreply.github.com>
2023-10-03 15:14:26 +0530

    Update SecurePassGen.py

--- SecurePassGen.py
+++ SecurePassGen.py
@@ -5,31 +5,28 @@ import string
 def generate_password(length):
     # Check if the length is less than 8 characters
     if length < 8:
-        # Raise an exception if length is less than 8 characters
-        raise Exception("Password length should be at least 8 characters")
+        raise ValueError("Password length should be at least 8 characters")
 
-    # Store the lowercase characters
+    # Define character sets
     lowercase_chars = string.ascii_lowercase
-
-    # Store the uppercase characters
     uppercase_chars = string.ascii_uppercase
-
-    # Store the digits
     digits_chars = string.digits
-
-    # Store the special characters
     special_chars = "!@#$%^&*"
 
-    # Choose one random character from each set
-    password = random.choice(lowercase_chars) + random.choice(uppercase_chars) + random.choice(digits_chars) + random.choice(special_chars)
+    # Create a password by selecting one character from each character set
+    password = (
+        random.choice(lowercase_chars)
+        + random.choice(uppercase_chars)
+        + random.choice(digits_chars)
+        + random.choice(special_chars)
+    )
 
-    # Combine the sets of characters
+    # Combine the character sets
     password_chars = lowercase_chars + uppercase_chars + digits_chars + special_chars
 
     # Choose random characters to complete the password
     password += ''.join(random.choice(password_chars) for i in range(length - 4))
 
-    # Return the password
     return password
 
 # Define a function to check password strength
@@ -40,7 +37,7 @@ def check_password_strength(password):
     has_digit = any(char.isdigit() for char in password)
     has_special = any(char in "!@#$%^&*" for char in password)
 
-    # Assess password strength
+    # Assess password strength based on length and character set criteria
     if length >= 16 and (has_lowercase and has_uppercase) and (has_digit and has_special):
         return "Super Strong"
     elif length >= 12 and (has_lowercase and has_uppercase) and (has_digit and has_special):
@@ -50,15 +47,23 @@ def check_password_strength(password):
     else:
         return "Weak"
 
-# Ask the user for the length of the password
-length = int(input("Enter the length of the password (at least 8): "))
+while True:
+    try:
+        # Ask the user for the length of the password
+        length = int(input("Enter the length of the password (at least 8): "))
 
-# Call the generate_password function with the user input length
-password = generate_password(length)
+        # Generate a password
+        password = generate_password(length)
+        
+        # Print the generated password
+        print("Generated Password:", password)
 
-# Print the generated password
-print("Generated Password:", password)
-
-# Check and print the password strength
-strength = check_password_strength(password)
-print("Password Strength:", strength)
+        # Check and print the password strength
+        strength = check_password_strength(password)
+        print("Password Strength:", strength)
+        
+        # Exit the loop if password generation is successful
+        break
+    except ValueError as e:
+        # Handle invalid input with an error message
+        print(f"Error: {e}. Please enter a valid password length.")
