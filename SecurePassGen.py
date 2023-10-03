7e68f885997c454449febc9b1e04c6150c33b446
Pushkar Sharma <96145925+Push4ck@users.noreply.github.com>
2023-10-03 15:09:39 +0530

    Update SecurePassGen.py

--- SecurePassGen.py
+++ SecurePassGen.py
@@ -1,7 +1,6 @@
 import random
 import string
 
-
 # Define the generate_password function
 def generate_password(length):
     # Check if the length is less than 8 characters
@@ -33,6 +32,23 @@ def generate_password(length):
     # Return the password
     return password
 
+# Define a function to check password strength
+def check_password_strength(password):
+    length = len(password)
+    has_lowercase = any(char.islower() for char in password)
+    has_uppercase = any(char.isupper() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+    has_special = any(char in "!@#$%^&*" for char in password)
+
+    # Assess password strength
+    if length >= 16 and (has_lowercase and has_uppercase) and (has_digit and has_special):
+        return "Super Strong"
+    elif length >= 12 and (has_lowercase and has_uppercase) and (has_digit and has_special):
+        return "Strong"
+    elif length >= 8 and (has_lowercase or has_uppercase) and (has_digit or has_special):
+        return "Moderate"
+    else:
+        return "Weak"
 
 # Ask the user for the length of the password
 length = int(input("Enter the length of the password (at least 8): "))
@@ -41,4 +57,8 @@ length = int(input("Enter the length of the password (at least 8): "))
 password = generate_password(length)
 
 # Print the generated password
-print("Generated Password: ", password)
+print("Generated Password:", password)
+
+# Check and print the password strength
+strength = check_password_strength(password)
+print("Password Strength:", strength)
