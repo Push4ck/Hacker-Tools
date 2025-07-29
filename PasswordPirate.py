be131e33353aee50cdf442f5142dffdea74ae584
Pushkar Sharma <96145925+Push4ck@users.noreply.github.com>
2025-07-29 15:29:50 +0530

    Update PasswordPirate.py

--- PasswordPirate.py
+++ PasswordPirate.py
@@ -5,29 +5,32 @@ import string
 import sys
 
 
-def generate_passwords(keywords, min_length, max_length,
-                       use_special_chars=False, use_numbers=False):
-    """Generate unique passwords from the given keywords and options."""
-    for n in range(min_length, max_length + 1):
-        for keyword in keywords:
+def generate_keyword_passwords(keywords, min_length, max_length):
+    """Generate base passwords using keyword combinations."""
+    for keyword in keywords:
+        for n in range(min_length, max_length + 1):
             if len(keyword) >= n:
                 yield keyword[:n]
             else:
-                for combination in itertools.product(keyword, repeat=n):
-                    yield ''.join(combination)
-    if use_special_chars or use_numbers:
-        for password in generate_passwords(keywords, min_length, max_length):
-            for n in range(1, len(password)):
-                if use_special_chars:
-                    for special_char in string.punctuation:
-                        yield f"{password[:n]}{special_char}{password[n:]}"
-                if use_numbers:
-                    for number in string.digits:
-                        yield password[:n] + number + password[n:]
+                for combo in itertools.product(keyword, repeat=n):
+                    yield ''.join(combo)
 
 
-def generate_password(length, use_special_chars=False, use_numbers=False):
-    """Generate a random password of the given length."""
+def mutate_passwords(passwords, use_special_chars, use_numbers):
+    """Mutate passwords with special characters and/or numbers."""
+    for pwd in passwords:
+        yield pwd  # base password
+        for i in range(1, len(pwd)):
+            if use_special_chars:
+                for c in string.punctuation:
+                    yield pwd[:i] + c + pwd[i:]
+            if use_numbers:
+                for d in string.digits:
+                    yield pwd[:i] + d + pwd[i:]
+
+
+def generate_random_password(length, use_special_chars=False, use_numbers=False):
+    """Generate one random password of specified length."""
     chars = string.ascii_letters
     if use_special_chars:
         chars += string.punctuation
@@ -36,39 +39,38 @@ def generate_password(length, use_special_chars=False, use_numbers=False):
     return ''.join(random.choice(chars) for _ in range(length))
 
 
-def parse_args(args):
-    """Parse command line arguments."""
-    parser = argparse.ArgumentParser('Generate passwords from keywords.')
-    parser.add_argument('--keywords', metavar='KEYWORD', nargs='+',
-                        help='keywords to use in password generation')
-    parser.add_argument('--min-length', metavar='MIN LENGTH', type=int,
-                        default=1,
-                        help='minimum length of the password (default: 1)')
-    parser.add_argument('--max-length', metavar='MAX LENGTH', type=int,
-                        default=10,
-                        help='maximum length of the password (default: 10)')
-    parser.add_argument('--special-chars', action='store_true', default=False,
-                        help='use special characters in the password')
-    parser.add_argument('--numbers', action='store_true', default=False,
-                        help='use numbers in the password')
-    return parser.parse_args(args)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Password Generator Tool')
+    parser.add_argument('--keywords', nargs='+', help='Keywords to use for password generation')
+    parser.add_argument('--min-length', type=int, default=4, help='Minimum password length')
+    parser.add_argument('--max-length', type=int, default=8, help='Maximum password length')
+    parser.add_argument('--special-chars', action='store_true', help='Include special characters')
+    parser.add_argument('--numbers', action='store_true', help='Include numbers')
+    parser.add_argument('--random', action='store_true', help='Generate a random password instead')
+    parser.add_argument('--length', type=int, help='Length of random password (required if --random)')
+    parser.add_argument('--output', type=str, required=True, help='Output file path')
+    return parser.parse_args()
 
 
-def main(args):
-    """Generate passwords from command line arguments and print them to stdout."""
-    opts = parse_args(args)
+def main():
+    opts = parse_args()
 
-    # Ask user for the file path to write the passwords to
-    output_file = input("Enter the file path to write the passwords to: ")
-
-    # Write passwords to the output file
-    with open(output_file, 'w') as f:
-        for password in generate_passwords(opts.keywords, opts.min_length,
-                                           opts.max_length,
-                                           use_special_chars=opts.special_chars,
-                                           use_numbers=opts.numbers):
+    with open(opts.output, 'w') as f:
+        if opts.random:
+            if not opts.length:
+                print("Error: --length is required when using --random")
+                sys.exit(1)
+            password = generate_random_password(opts.length, opts.special_chars, opts.numbers)
             f.write(password + '\n')
+        else:
+            if not opts.keywords:
+                print("Error: --keywords is required unless --random is specified")
+                sys.exit(1)
+            base_passwords = generate_keyword_passwords(opts.keywords, opts.min_length, opts.max_length)
+            final_passwords = mutate_passwords(base_passwords, opts.special_chars, opts.numbers)
+            for pwd in final_passwords:
+                f.write(pwd + '\n')
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
